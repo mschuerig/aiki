ce99b10134a1fdf356ad89b2147839ae09d38d0f
Michael Schuerig <michael@schuerig.de>
2009-05-10 00:25:15 +0200

    Removed debug output.

--- Standby.js
+++ Standby.js
@@ -11,14 +11,12 @@ dojo.declare('aiki.Standby', null, {
   },
 
   start: function() {
-    console.debug('*** START standby ', this._targetNode);
     var standbyNode = dojo.create('div', {}, dojo.body());
     this._standby = new dojox.widget.Standby({ target: this._targetNode }, standbyNode);
     this._standby.show();
   },
 
   stop: function() {
-    console.debug('*** STOP standby ', this._targetNode);
     if (this._standby) {
       this._standby.hide();
       this._standby.destroyRecursive();
