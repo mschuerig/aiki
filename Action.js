8c1791536c79a05c10f4eee0c776c21e4c5ac7d7
Michael Schuerig <michael@schuerig.de>
2009-05-17 21:48:43 +0200

    Move context menu and action handling to _QueriedListMixin.

--- Action.js
+++ Action.js
@@ -2,10 +2,10 @@ dojo.provide('aiki.Action');
 
 //### TODO API description
 dojo.declare('aiki.Action', null, {
-  constructor: function(label, execute, enabled) {
+  constructor: function(/*String*/label, /*Function*/execute, /*Boolean?*/enabled) {
     this.label = label;
     this.execute = execute;
-    this.enabled = (arguments.length >= 3) ? enabled : true;
+    this.enabled = enabled !== false;
     this.disabled = !this.enabled;
   }
 });
--- _QueriedListMixin.js
+++ _QueriedListMixin.js
@@ -1,8 +1,20 @@
 dojo.provide('aiki._QueriedListMixin');
+dojo.require('dojo._base');
+dojo.require('dojo._base.event');
+dojo.require('dijit.Menu');
+dojo.require('dijit.MenuItem');
+dojo.require('dijit.MenuSeparator');
+dojo.require('dijit.Tooltip');
+dojo.require('aiki.array');
+dojo.require('aiki.Action');
 dojo.require('aiki.QueryParser');
 dojo.require('aiki.QueryHelp');
 
 dojo.declare('aiki._QueriedListMixin', null, {
+
+  actions: null,
+  _contextItemName: 'item',
+
   _initGrid: function(grid, props) {
     grid.attr('structure', props.gridStructure);
     grid.setSortInfo(props.sortInfo);
@@ -49,6 +61,43 @@ dojo.declare('aiki._QueriedListMixin', null, {
     }
   },
 
+  _gridContextMenu: function(grid, actionFactory) {
+    actionFactory = actionFactory || dojo.hitch(this, '_getActions');
+    var menu = this._contextMenu = (this._contextMenu || new dijit.Menu());
+    var itemName = this._contextItemName;
+
+    var showMenu = function(e) {
+      var context = {};
+      context[itemName] = e.grid.getItem(e.rowIndex);
+      var actions = actionFactory(context, e);
+      if (actions && actions.length > 0) {
+        dojo.forEach(actions, function(action, i) {
+          if (action === '-') {
+            if (i !== 0 && i !== actions.length - 1) {
+              menu.addChild(new dijit.MenuSeparator());
+            }
+          } else if (dojo.isString(action)) {
+            menu.addChild(new dijit.MenuItem({
+              label:    action,
+              disabled: true
+            }));
+          } else {
+            menu.addChild(new dijit.MenuItem({
+              label:    action.label,
+              onClick:  dojo.partial(action.execute, context),
+              disabled: action.disabled
+            }));
+          }
+        });
+        menu.startup();
+        menu._openMyself(e);
+      }
+    };
+
+    dojo.connect(grid, 'onCellContextMenu', showMenu);
+    dojo.connect(menu, 'onClose', menu, 'destroyDescendants');
+  },
+
   _gridTooltips: function(grid, messageCallback) {
     var showTooltip = function(e) {
       var item = e.grid.getItem(e.rowIndex);
@@ -78,6 +127,32 @@ dojo.declare('aiki._QueriedListMixin', null, {
       label: help.domNode.innerHTML,
       position: ['below', 'above']
     });
-  }
+  },
 
+  _setActionsAttr: function(actions) {
+    this.actions = [].concat(actions);
+  },
+
+  _addTopAction: function(label, execute, enabled) {
+    this._topActions = this._topActions || [];
+    if (execute) {
+      this._topActions.push(new aiki.Action(label, execute, enabled));
+    } else {
+      this._topActions.push(label);
+    }
+  },
+
+  _getActions: function(context) {
+    var actionSpecs = (this._topActions || []).concat(this.actions || []);
+    var actions = [];
+    for (var i = 0, l = actionSpecs.length; i < l; i++) {
+      var action = actionSpecs[i];
+      if (dojo.isFunction(action)) {
+        actions = actions.concat(action(context));
+      } else {
+        actions.push(action);
+      }
+    }
+    return actions;
+  }
 });
