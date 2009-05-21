be7c0d6fdcee995c75291a77468842786c9917e3
Michael Schuerig <michael@schuerig.de>
2009-05-21 04:47:18 +0200

    SortedList/Item: added NLS.

--- _SortedList/Item.js
+++ _SortedList/Item.js
@@ -1,10 +1,13 @@
 dojo.provide('aiki._SortedList.Item');
 dojo.require('dojo.html');
+dojo.require('dojo.i18n');
 dojo.require('dijit._Templated');
 dojo.require('dijit._Widget');
 dojo.require('dijit.form.Button');
+dojo.requireLocalization('aiki', 'common');
 
 dojo.declare('aiki._SortedList.Item', [dijit._Widget, dijit._Templated], {
+  //### i18n for label/title
   baseClass: 'aikiSortedListItem',
   templatePath: dojo.moduleUrl('aiki', '_SortedList/Item.html'),
   widgetsInTemplate: true,
@@ -14,6 +17,7 @@ dojo.declare('aiki._SortedList.Item', [dijit._Widget, dijit._Templated], {
 
   postMixInProperties: function() {
     this.removeButtonClass = this.baseClass + 'RemoveButton';
+    this._nls = dojo.i18n.getLocalization('aiki', 'common');
   },
 
   postCreate: function() {
